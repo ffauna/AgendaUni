4204389ea17490d42a4e33e803862f00e16b3562
Heisengkey <35464406+Heisengkey@users.noreply.github.com>
2020-11-12 20:37:32 +0100

    Delete menu.php

--- menu.php
+++
@@ -1,6 +0,0 @@
-<div class="topnav">
-    <a class="home" href="/AgendaUni/login.php"><img src="img/logo.png" height="30" width="40"></a>
-    <div class="botones_menu">
-        <a href="about.php"></a>
-    </div>
-</div>
