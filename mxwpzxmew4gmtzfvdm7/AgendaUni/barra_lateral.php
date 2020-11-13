d2a445486f39320a680e30faf2149526f2bb5a10
Heisengkey <pedromvipe@gmail.com>
2020-11-13 18:29:48 +0100

    Limpieza del repositorio

--- mxwpzxmew4gmtzfvdm7/AgendaUni/barra_lateral.php
+++
@@ -1,7 +0,0 @@
-<div class="lateralnav">
-
-    <a href="/AgendaUni/perfil.php" class="lateral-icon" id="perfil"><i class="fas fa-user-circle"></i></a>
-    <a href=# class="lateral-icon" id="notificacion"><i class="fas fa-bell"></i></a>
-    <a href=# class="lateral-icon" id="ajustes"><i class="fas fa-cog"></i></a>
-    <a href=# class="lateral-icon" id="calendarios"><i class="fas fa-calendar-alt"></i></a>
-</div>
